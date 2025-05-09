46a2cfff71d621e1fa115f4064f136bbeda931dd
Benjamin Inglis <bjammin134@gmail.com>
2025-05-09 11:03:12 -0700

    Add default "No Project Selected" component Handle default case where no project is selected. Display input fields to user when one of the "create project" buttons is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
 function App() {
+  // selectedProjectId set to undefined => do nothing, null => adding a new project
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevProjectsState) => {
+      return {
+        ...prevProjectsState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
